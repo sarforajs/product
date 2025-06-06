c4afaf55d31bd5918a80ee9f1fa58f6309e96e34
sarforajs <128588277+sarforajs@users.noreply.github.com>
2025-06-06 22:00:58 +0530

    Update ProductController.java

---
+++ src/main/java/com/sarforaj/productservice/controller/ProductController.java
@@ -0,0 +1,33 @@
+package com.sarforaj.productservice.controller;
+
+import com.sarforaj.productservice.model.Product;
+import com.sarforaj.productservice.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return service.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProduct(@PathVariable Long id) {
+        return service.getProductById(id);
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return service.createProduct(product);
+    }
+}
